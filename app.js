f846160466bc5581c05d99a740549c0552d96719
Diegotes-DH <testadiegom@gmail.com>
2021-05-27 20:13:47 -0300

    update

--- app.js
+++ app.js
@@ -8,3 +8,8 @@ app.listen(3000, ()=>{console.log("Server start in: http://localhost:3000/")});
 
 app.get("/", (req, res)=>{res.sendFile(path.join(__dirname,"views", "home.html"))});
 
+app.get("/register", (req, res)=>{res.sendFile(path.join(__dirname,"views", "register.html"))});
+
+app.get("/login", (req, res)=>{res.sendFile(path.join(__dirname,"views", "login.html"))});
+
+
